8b12539e31f5cedf90d8dc4c4bd1f27d52e3c14d
MARTIN EDOGBO <EDOGBOMARTIN38@gmail.com>
2024-04-12 23:11:11 +0100

    new stuff

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+/* import { useState, useEffect } from "react"; */
+import BlogList from "./BlogList";
+import useFetch from "./usefetch";
+
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch("http://localhost:4000");
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {/*       <BlogList blogs={blogs.filter((blog) => blog.author === "martin")} title="Martin's Blogs" /> */}
+    </div>
+  );
+};
+
+export default Home;
