390365dd347f276895f9a120080fdb7dfa0614b6
MARTIN EDOGBO <EDOGBOMARTIN38@gmail.com>
2024-04-18 15:34:29 +0100

    brand new

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./usefetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data: blog, isPending, error } = useFetch("http://localhost:7000/data/");
+  const { data: blog, isPending, error } = useFetch("http://localhost:7000/data/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:7000/data/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -14,6 +23,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
